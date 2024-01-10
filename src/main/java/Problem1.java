b1d20278eb293a67f6b2d36d3e8ddcb2413e8122
vincentp <vincentp@outlook.com>
2024-01-10 12:40:26 -0800

    problem 1 and 3 code

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,11 +1,44 @@
+
+/**
+ * Vincent Pham
+ * problem 1
+ *
+ * CSS 143
+ * - Assigned: 1/03/2024
+ * - Due: Saturday January 13 23:59:00 PST 2024
+ */
+
+
 public class Problem1 {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
 
-    // Do not change signature (function name, parameter variable type and return type)
-    public static int binarySearch(int[] data, int target) {
-        // TODO: Add your solution
-        return -1; // Placeholder. TODO: Replace this line with your own code.
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;  // if the target is not found in the array
     }
-    // TODO: answers "how would you prove your code is correct?". "ChatGPT says so" doesn't count as a good answer.
 
-    // TODO: remove all the comment of "TODOs" and "Placeholder" once the homework is done. In other files too.
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 6, 8, 10, 12, 15};
+        int target = 8;
+
+        int result = binarySearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Target " + target + " found at index " + result);
+        } else {
+            System.out.println("Target " + target + " not found in the array");
+        }
+    }
 }
--- src/main/java/Problem3.java
+++ src/main/java/Problem3.java
@@ -1,12 +1,60 @@
+/**
+ * Vincent Pham
+ * problem 3
+ *
+ * CSS 143
+ * - Assigned: 1/03/2024
+ * - Due: Saturday January 13 23:59:00 PST 2024
+ */
+
 public class Problem3 {
 
-    // Do not change signature (function name, parameters, return type)
-    public static long SumOfNonUnique(int[] data) {
-        // TODO: Add your solution
-        return -1; // Placeholder. TODO: Replace this line with your own code.
+    public static int sumOfNonUnique(int[] arr) {
+        int sum = 0;
+
+        // find the range of integers in the array
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            //finds the max and min values with in the array
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+
+        // creates an array to count the occurrences of each element
+        int[] count = new int[max - min + 1];
+
+
+        // updates the count array based on the occurrences of each element in the input array
+        for (int num : arr) {
+            count[num - min]++;
+        }
+
+
+        // calculates the sum of non-unique elements
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > 1) {
+                sum += (i + min) * count[i];
+            }
+        }
+
+        return sum;
     }
 
-    /*
-    TODO: Bonus Points. Add your answer here.
-     */
+    public static void main(String[] args) {
+        // test cases
+        int[] test1 = {3};
+        int[] test2 = {3, 2};
+        int[] test3 = {2, 3, 2};
+        int[] test4 = {2, 4, 2, 1, 4, -5, 4};
+        int[] test5 = {1, 1, 1, 2, 2, 3, 3, 4, 4};
+
+        System.out.println("Test 1: " + sumOfNonUnique(test1)); // Output: 0
+        System.out.println("Test 2: " + sumOfNonUnique(test2)); // Output: 0
+        System.out.println("Test 3: " + sumOfNonUnique(test3)); // Output: 4
+        System.out.println("Test 4: " + sumOfNonUnique(test4)); // Output: 16
+        System.out.println("Test 5: " + sumOfNonUnique(test5)); // Output: 20
+    }
 }
